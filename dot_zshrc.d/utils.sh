251161b49cf703df582996fb5a5f07a20c66d240
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2025-12-22 21:22:36 +0700

    use nuscript for some command in zsh

--- dot_zshrc.d/utils.sh
+++ dot_zshrc.d/utils.sh
@@ -13,8 +13,31 @@ alias ipd='ip addr show'
 alias fwd='sudo firewall-cmd'
 alias pxc='proxychains -q'
 
+is_nu_exist=$(command -v nu 2> /dev/null);
+
+
 # Disk utils
-alias dug='du -h * --max-depth=0 | grep -P "G\t"'
+unalias ls;
+# using nu command for ls.
+if [[ "$is_nu_exist" != "" ]];then
+    ls() {
+        ls_arg=$(echo -n $@ | sed 's#:#|#g')
+        nu -c "ls $ls_arg"
+        # echo $@
+        # echo $ls_arg
+    }
+    lsd() {
+        nu -c "ls -da | sort-by size | print"
+    }
+fi
+
+dug() {
+    if [[ "$is_nu_exist" != "" ]];then
+        nu -c "ls -da | where size > 1GB | sort-by size | print"
+    else
+        du -h * --max-depth=0 | grep -P "G\t"
+    fi
+}
 # alias dud='du -hd 0 '
 dud() {
   if [[ "$*" == "" ]]; then
