504441e873f4258ed063ad4b786d965831025aa1
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2025-12-31 06:54:42 +0700

    update utils.sh

--- dot_zshrc.d/utils.sh
+++ dot_zshrc.d/utils.sh
@@ -3,7 +3,7 @@ alias fpk="flatpak"
 alias q="exit"
 alias psn='ps -o user,pid,ni,stat,start,time,cmd'
 alias y='yes'
-alias lss='ls -Ssrh'
+alias lss='du -sh * | sort -h'
 
 # Shorter commands for any program.
 alias pod='podman'
@@ -11,6 +11,17 @@ alias podc='podman-compose'
 alias ipd='ip addr show'
 alias fwd='sudo firewall-cmd'
 alias pxc='proxychains -q'
+
+# Podman
+lpod() {
+  for i in $(podman images | grep local | awk '{print $1}'); do
+    print "$i"
+    if [[ "$1" == 'r' ]]; then
+      echo deleting...
+      podman rmi "$i"
+    fi
+  done
+}
 
 is_nu_exist=$(command -v nu 2> /dev/null);
 
@@ -23,25 +34,21 @@ lst() {
   fi
 }
 # Disk utils
-unalias ls;
 # using nu command for ls.
 if [[ "$is_nu_exist" != "" ]];then
-    ls() {
-        ls_arg=$(echo -n $@ | sed 's#:#|#g')
-        nu -c "ls $ls_arg"
-    }
-    lsd() {
+   lsd() {
         nu -c "ls -da | sort-by size | print"
     }
+else
+  lsd () {
+    if [[ "$1" == "" ]]; then
+      du -sh * | sort -h
+    else
+      du -sh $1 | sort -h
+    fi
+  }
 fi
 
-dug() {
-    if [[ "$is_nu_exist" != "" ]];then
-        nu -c "ls -da | where size > 1GB | sort-by size | print"
-    else
-        du -h * --max-depth=0 | grep -P "G\t"
-    fi
-}
 # alias dud='du -hd 0 '
 dud() {
   if [[ "$*" == "" ]]; then
@@ -51,13 +58,3 @@ dud() {
   fi
 }
 
-# Podman
-lpod() {
-  for i in $(podman images | grep local | awk '{print $1}'); do
-    print "$i"
-    if [[ "$1" == 'r' ]]; then
-      echo deleting...
-      podman rmi "$i"
-    fi
-  done
-}
